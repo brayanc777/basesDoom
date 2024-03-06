d81d07af80389e888eb9cc5e38d3ecc16aa7647e
brayanc777 <130773223+brayanc777@users.noreply.github.com>
2024-03-06 16:49:58 -0500

    Add files via upload

---
+++ doomBrayanCastrillon/js/script.js
@@ -0,0 +1,29 @@
+// let header = document.getElementById("header")
+// let logo = document.getElementById("logo")
+// logo.alt = 'texto alternativo desde javScript'
+// logo.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png'
+// let enlaces = document.getElementsByClassName("enlaces")
+// enlaces[0].textContent = 'esto es un enlace'
+// enlaces[0].href = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png'
+// enlaces[0].target = '_blank'
+// function mostrarImagen(){
+//     let logo = document.getElementById("logo")
+//     logo.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png'
+//     return logo
+// }
+// function borrarImagen(logo){
+//     logo = document.getElementById("logo")
+//     logo.src =''
+// }
+//mostrarImagen()
+
+function login(){
+    let correo = document.getElementById('correo')
+    let contrasena = document.getElementById('contrasena')
+    correo.value == 'brayan' && contrasena.value == '321'
+    location.href = 'https://librefutbol.su/eventos/?r=aHR0cHM6Ly9mbG93cml2ZS5hZ2VuY3kvc3Rhcl9qd3AucGhwP2dldD1odHRwczovL2xpdmUtZnRjLW5hLXNvdXRoLTIubWVkaWEuc3Rhcm90dC5jb20vZ3J1MS9xYjAxL3N0YXJwbHVzL2V2ZW50LzIwMjQvMDMvMDYvTWFuY2hlc3Rlcl9DaXR5X3ZzX0NvcGVuaF8yMDI0MDMwNl8xNzA5NzQ4NjQxMDI1L2N0ci1hbGwtY29tcGxldGUubTN1OCZpbWc9aHR0cHM6Ly9wcm9kLXJpcGN1dC1kZWxpdmVyeS5kaXNuZXktcGx1cy5uZXQvdjEvdmFyaWFudC9zdGFyL0U5NEU5MkVDNjc4OTgyMjdCRTZCNzIyMDkxMzc3MTlGNTlCNkNEMEUxRkUzMTUxNzFEMzgwNzk1NTVGRDE2ODMvc2NhbGU/d2lkdGg9MTkyMCZhc3BlY3RSYXRpbz0xLjc4JmZvcm1hdD1qcGVnJmtleT0xMDEyODE0ZGQ4ZTE0OGIwOTNmNmE4Mjc2YWQ4NmY2YiZrZXkyPTUwY2UwOTNlZjI4YWM1MDM3MzdiMjBhMGNlODcyMDU1'
+}
+// function iniciars(){
+//     let iniciar = document.getElementById("iniciar")
+//     iniciar = login()
+// }
